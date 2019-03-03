9b17833a78556afacafa7a4c92866acea349de8f
Maximo DeLaRosa <mjd809@gmail.com>
2019-03-03 18:20:14 -0500

    fixed logout button

--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -35,10 +35,9 @@ class SideBar extends Component {
             <p><NavLink exact to='/convo'><i className="fas fa-comments fa-fw" />Conversations</NavLink></p>
             <p><NavLink exact to="/"><i className="fas fa-sticky-note fa-fw" />View Your Notes</NavLink></p>
             <p><NavLink exact to='/create'><i className="fas fa-plus fa-fw" />Create New Note</NavLink></p>
-            <p
-              style={{ cursor: 'pointer', color: 'rgb(129, 129, 129)' }}
-              onClick={e => this.logoutUser(e)}
-            ><NavLink exact to='/create'><i className="fas fa-sign-out-alt fa-fw" />Sign Out</NavLink>
+            <p className="icon-hover"onClick={e => this.logoutUser(e)}
+            >
+              <i className="fas fa-sign-out-alt fa-fw" />Sign Out 
             </p>
           </div>
         </div>
